2905bafb0bb93ce452fbc5d84714288bdf518c7d
Kotikov <11deadsluts@gmail.com>
2023-02-03 19:48:31 +0300

    delete contacts is add

---
+++ delete_contacts.py
@@ -0,0 +1,12 @@
+import os
+
+def delete_contact ():
+    with open('phone_book.csv') as pb, open('new_phonebook.csv', 'w') as npb :
+        search = input('Whose contact do you want to delete?: ')
+        for line in pb:
+            if search not in line:
+                npb.write(line)
+    os.remove('phone_book.csv')
+    os.rename('new_phonebook.csv', 'phone_book.csv')
+        
+delete_contact()

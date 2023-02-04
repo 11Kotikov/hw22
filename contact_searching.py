90cd8ba1ed6f31d1950d21edf02464290f8b5555
Kotikov <11deadsluts@gmail.com>
2023-02-04 16:51:35 +0300

    contact searching is ready

--- contact_searching.py
+++ contact_searching.py
@@ -1,3 +1,6 @@
+import os
+
+
 def open_full_book():
     with open('phone_book.csv') as pb:
         while True:
@@ -8,12 +11,16 @@ def open_full_book():
 
 
 def contact_searching():
-
-    with open('phone_book.csv') as pb:
-        search = input('Whose contact do you want to find?: ')
-        lines = pb.readlines()
-        for i, line in enumerate(lines):
-            if search in line:
-                print('|{}| {}'.format(i+1, line))
-            else:
-                print('There are no such key-words, please check your input (including capital letters) and try again')
+    empty = 'phone_book.csv'
+    if os.path.getsize(empty) == 0:
+        print('The phone book is empty, please add at least one contact')
+    else:
+        with open('phone_book.csv') as pb:
+            search = input('Whose contact do you want to find?: ')
+            lines = pb.readlines()
+            for i, line in enumerate(lines):
+                if search in line:
+                    print('|{}| {}'.format(i+1, line))
+                else:
+                    print(
+                        f'There are no such key-words as the "{search}", please check your input (including capital letters) and try again or chose "2. Show all contacts"')

9f7d3a3f0e998c5ed85c2daf65f50a175942a500
Kotikov <11deadsluts@gmail.com>
2023-02-08 00:42:50 +0300

    input checking is done

--- input_checking.py
+++ input_checking.py
@@ -1,7 +1,11 @@
-def input_check ():
+def input_check():
     try:
         input_number = int(input('Please, choose one number from the menu: '))
-        return input_number
+        if input_number < 0 or input_number > 9:
+            print('Error: Wrong choise')
+            return input_check()
+        else:
+            return input_number
     except ValueError:
         print('Error: Input a number, please')
         return input_check()
